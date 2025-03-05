376edb8c4a6a8a317fe169b24213d17e879e7cfb
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-03-05 08:55:39 -0500

    Initial Commit

---
+++ QuoteFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuoteFinder
+//
+//  Created by Sebastian on 2025-03-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuoteFinder/QuoteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteFinderApp.swift
+//  QuoteFinder
+//
+//  Created by Sebastian on 2025-03-05.
+//
+
+import SwiftUI
+
+@main
+struct QuoteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
