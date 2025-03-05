683e759136c50030f668c109e0803d4c7b18e087
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-03-05 09:49:14 -0500

    created a working app that retreives data from the endpoint, some problems with the quotes though as they do not always decode

--- QuoteFinder/Views/QuoteView.swift
+++ QuoteFinder/Views/QuoteView.swift
@@ -12,7 +12,13 @@ struct QuoteView: View {
     // MARK: Stored properties
     
     // Create the view model (temporarily show the default joke)
-    @State var viewModel = QuoteViewModel(currentQuote: exampleQuote)
+    @State var viewModel = QuoteViewModel()
+    
+    // Controls button visibility
+    @State var buttonOpacity = 0.0
+    
+    // Starts a timer to wait on revealing button to get new joke
+    @State var buttonTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
     
     // MARK: Computed properties
     var body: some View {
@@ -31,6 +37,39 @@ struct QuoteView: View {
                 .font(.title)
                 .multilineTextAlignment(.center)
                 
+                
+                Button {
+                 
+                    // Hide punchline and button
+                    withAnimation {
+                        viewModel.currentQuote = nil
+                        buttonOpacity = 0.0
+                    }
+                                        
+                    // Get a new joke
+                    Task {
+                        await viewModel.fetchQuote()
+                    }
+                    
+                    // Restart timers
+                    buttonTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+                    
+                } label: {
+                 
+                    Text("New Quote")
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .opacity(buttonOpacity)
+                .onReceive(buttonTimer) { _ in
+                    
+                    withAnimation {
+                        buttonOpacity = 1.0
+                    }
+                    
+                    // Stop the timer
+                    buttonTimer.upstream.connect().cancel()
+                }
             }
             
         }
